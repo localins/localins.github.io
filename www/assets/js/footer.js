1d54fa80f0e7f8adf6b21d9f3e6c7f7452db20ff
Can't Sleep <localamnesiac@gmail.com>
2026-06-25 21:41:52 +0300

    Good sanity practices

--- www/assets/js/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document
-        .getElementById("last-updated")
-        .appendChild(
-            document.createTextNode("Last updated: " + document.lastModified),
-        );
-});
